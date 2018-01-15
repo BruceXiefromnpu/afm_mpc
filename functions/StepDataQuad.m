0aaa716da93c35c29db1c2eff7161b18345707d4
arnold <abraker@fastmail.com>
2018-01-14 21:00:01 -0700

    start objectifying everything

---
+++ functions/StepDataQuad.m
@@ -0,0 +1,18 @@
+classdef StepDataQuad
+    
+    properties
+        % params;
+        % file;
+        % fig_files;
+        % savedata;
+        % results;
+    end
+    
+    methods
+        function self = StepDataQuad(Params, varargin)
+            self = self@StepData(Params, varargin{:})
+        end
+        
+    end
+    
+end
