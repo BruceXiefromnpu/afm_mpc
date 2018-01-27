aef07c7d32dfdd504a1b714b26a0e3de21443a0f
arnold <abraker@fastmail.com>
2018-01-26 19:38:10 -0700

    better docs in change current figure.

--- functions/change_current_figure.m
+++ functions/change_current_figure.m
@@ -1,3 +1,11 @@
+% change_current_figure(h)
+% 
+% Change current figure without changing focuse to it;
+%
+% Inputs
+% ------
+%   h :  Handle to the figure you want to plot to.
+
 function change_current_figure(h)
     set(0,'CurrentFigure',h)
 end
