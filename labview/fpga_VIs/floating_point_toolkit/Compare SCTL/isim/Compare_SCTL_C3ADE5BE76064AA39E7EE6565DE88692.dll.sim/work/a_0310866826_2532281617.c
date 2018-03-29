882d9bcaad2dc8003d97ab650c2c00fbb1221ef9
Roger Braker <abraker@fastmail.com>
2018-03-29 16:34:42 -0600

    We should be tracking the labview folder in here

---
+++ labview/fpga_VIs/floating_point_toolkit/Compare SCTL/isim/Compare_SCTL_C3ADE5BE76064AA39E7EE6565DE88692.dll.sim/work/a_0310866826_2532281617.c
@@ -0,0 +1,31 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+/* This file is designed for use with ISim build 0x8ef4fb42 */
+
+#define XSI_HIDE_SYMBOL_SPEC true
+#include "xsi.h"
+#include <memory.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+
+
+
+
+extern void work_a_0310866826_2532281617_init()
+{
+	xsi_register_didat("work_a_0310866826_2532281617", "isim/Compare_SCTL_C3ADE5BE76064AA39E7EE6565DE88692.dll.sim/work/a_0310866826_2532281617.didat");
+}
