54f359bbe092fa07bcb0ec46381c8c59629c91b9
Roger Braker <abraker@fastmail.com>
2018-04-27 21:31:13 -0600

    better doc string for power_spectrum. Fix a small bug with plotting in settle_time

--- functions/power_spectrum.m
+++ functions/power_spectrum.m
@@ -1,15 +1,17 @@
-function [P1, freqs] = power_spectrum(x, Ts )
-%FFT_SPEC Summary of this function goes here
-%   Detailed explanation goes here
+function [pow_spec, freqs] = power_spectrum(x, Ts )
+  % [pow_spec, freqs] = power_spectrum(x, Ts )
+  % For a vector of data x at sample rate Ts,
+  % returns the power spectrum and associated frequencies.
+  % Roughly this is the modulus of the first half of the FFT of the signal.
 
-Fs = 1/Ts;
+  Fs = 1/Ts;
 
-Y = fft(x);
-L = length(x);
-P2 = abs(Y/L);
-P1 = P2(1:L/2+1);
-P1(2:end-1) = 2*P1(2:end-1);
-freqs = Fs*[0:L/2]/L;
+  Y = fft(x);
+  L = length(x);
+  P2 = abs(Y/L);
+  pow_spec = P2(1:L/2+1);
+  pow_spec(2:end-1) = 2*pow_spec(2:end-1);
+  freqs = Fs*[0:L/2]/L;
 
 end
 
