6e973eaf4c9bd439944b3121a0ee12e360dd2eda
arnold <abraker@fastmail.com>
2018-05-15 10:37:37 -0600

    make normalizing the dc-gain an option. But by default still do it.

--- functions/eject_gdrift.m
+++ functions/eject_gdrift.m
@@ -9,11 +9,13 @@
 %   dynamics.
 %   g_eject: a zpk model of the INVERSE low freq pole zero pair.
 
-function [sys_vibr, g_eject] = eject_gdrift(sys_nodelay)
+function [sys_vibr, g_eject] = eject_gdrift(sys_nodelay, normalize_dc)
     Ts = sys_nodelay.Ts;
     [wp_real_x, wz_real_x] = w_zp_real(sys_nodelay);
 %     rho_1 = wz_real_x(1)/wp_real_x(1);
     g_eject = zpk(exp(-wp_real_x(1)*Ts), exp(-wz_real_x(1)*Ts), 1, Ts);
-    g_eject = g_eject/dcgain(g_eject);
+    if ~exist('normalize_dc', 'var') || normalize_dc
+      g_eject = g_eject/dcgain(g_eject);
+    end
     sys_vibr = minreal(sys_nodelay*g_eject);
 end
