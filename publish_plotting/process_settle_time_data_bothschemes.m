e308bf8912817d2cba2ae083ac07128e6264fa96
arnold <abraker@fastmail.com>
2018-07-26 20:55:23 -0600

    more writing. make the settle-time points plot bigger. switch which steps I show Add a plotting script of the hysteresis fit.

--- publish_plotting/process_settle_time_data_bothschemes.m
+++ publish_plotting/process_settle_time_data_bothschemes.m
@@ -90,6 +90,7 @@ line_styles = {'-', '--', '-', '--','-', '--','-', '--','-', '--','-', '--','-',
 step_exps_CZ_cell = cell(1, length(files_choose_zet));
 for k=1:length(names_choose_zet)
   dat = load(fullfile(root_CZ, files_choose_zet{k}));
+  
   exp_name_str = fields(dat);
   exp_name_str = exp_name_str{1};
   dat = dat.(exp_name_str);
@@ -147,10 +148,19 @@ saveas(fig100, fullfile(PATHS.jfig(), 'ts_colorbar.svg'))
 % Lets try lucy's suggestion of plotting the settle-times vs |delta-ref|
 clc
 width = 3.5; 
-height = 2.5;
+height = 3.5;
 Fig15 = mkfig(15, width, height); clf
-ax1 = axes('Position', [0.13, 0.17, 0.85, 0.8]);
+% ax1 = axes('Position', [0.13, 0.17, 0.85, 0.8]);
+
+lft = 0.455;
+bt = 0.3;
+top_pad = 0.07 + 10/72;
+rt_pad = 0.07 ;
+wd = width - lft - rt_pad;
+ht = height - bt - top_pad;
+ax1 = axes('Units', 'inches', 'Position', [lft bt wd ht]);
 ms = {'d', '+', 'o', 'x'};
+colrs = {'k', 'k', 'k', 'k'};
 msize = 5;
 hands1 = gobjects(1, 4);
 hands2 = gobjects(1, 4);
@@ -158,57 +168,58 @@ idx = [5:8];
 j = 1;
 for k=1:4
   hands1 (k) = plot(abs(step_exps_CZ.step_ref.step_diff_amps(2:end)),...
-    step_exps_CZ.TS_mat(:, idx(k))*1000, ms{k}, 'Color', 'k', 'MarkerSize', msize);
+    step_exps_CZ.TS_mat(:, idx(k))*1000, ms{k}, 'Color', colrs{k}, 'MarkerSize', msize);
   hands1(k).DisplayName = step_exps_CZ.step_exps{idx(k)}.name;
   hold on
   
 end
-ylm1 = ylim();
+
 set(ax1, 'YScale', 'log')
 set(ax1, 'YTick', [2,5, 10,20, 50, 100], 'YLim', [0.001, 100])
 set(ax1, 'YLim', [0.001, 100])
 
 grid on
 ax_pos = ax1.Position;
-ax2 = axes('Position', ax_pos);
+% ax2 = axes('Units', 'inches', 'Position', ax_pos);
 % Fig15.CurrentAxes = ax2;
 
 for k=1:4
-  hands2 (k) = plot(ax2, abs(step_exps_CS.step_ref.step_diff_amps(2:end)),...
+  hands2 (k) = plot(ax1, abs(step_exps_CS.step_ref.step_diff_amps(2:end)),...
     step_exps_CS.TS_mat(:, idx(k))*1000, ms{k}, 'Color', 'r', 'MarkerSize', msize);
   hands2(k).DisplayName = step_exps_CS.step_exps{idx(k)}.name;
   hold on
 end
-ylm2 = ylim();
-set(ax2, 'YScale', 'log')
-set(ax2, 'Visible', 'off', 'YLim', [2, max([ylm1(2), ylm2(2)])]);
-set(ax1, 'Visible', 'on', 'YLim', [2, max([ylm1(2), ylm2(2)])]);
+ylm1 = ylim();
+set(ax1, 'Visible', 'on', 'YLim', [2, ylm1(2)]);
 
 grid on
-leg1 = legend(hands1);
-set(leg1, 'Box', 'off', 'Position', [0.1020 0.7220 0.3539 0.2362], 'FontSize', 8)
-pos1 = leg1.Position();
-
-leg2 = legend(hands2);
-pos2 = leg2.Position(); 
-%                                     lft              bt 
-set(leg2, 'Box', 'off', 'Position', [pos1(1)+ pos1(3), pos1(2), pos2(3:4)], 'FontSize', 8)
+leg1 = legend([hands1, hands2]);
+set(leg1, 'Box', 'on', 'FontSize', 8)
+leg_ht = 1.2388;
+leg_wd = 0.63;
+% pos1 = [0.6 height-leg_ht, leg_ht, leg_wd];
+set(leg1, 'Units', 'inches', 'NumColumns', 2, 'Position', [0.46323 2.7045 2.4402 0.59062])
 
 
 Fig15.CurrentAxes = ax1;
-xlabel('$|r_i - r_{i-1}|$')
-ylabel('settle time [ms]')
-%%
-if 1 %saveon
+xlab = xlabel('$|r_i - r_{i-1}|$');
+ylab = ylabel('settle time [ms]');
+htit = title('(experiment)', 'FontSize', 10);
+
+tick_pad_inches = ax1.FontSize/72 + 0.0;  % 1/72 = pt
+set(xlab, 'Units', 'inches', 'HorizontalAlignment', 'center',...
+  'Position', [wd/2, -tick_pad_inches, 0])
+
+if saveon
   saveas(Fig15, fullfile(PATHS.jfig, 'ts_vs_delref.svg'))
 end
-%%
+
 % Again try lucy's suggestion of plotting the settle-times vs |delta-ref|.
 % This time for the simulations.
-width = 3.5; 
-height = 2.5;
 Fig16 = mkfig(16, width, height); clf
-ax1 = axes('Position', [0.13, 0.17, 0.85, 0.8]);
+
+ax3 = axes('Units', 'inches', 'Position', [lft bt wd ht]);
+
 ms = {'d', '+', 'o', 'x'};
 msize = 5;
 hands1 = gobjects(1, 4);
@@ -222,41 +233,38 @@ for k=1:4
   hold on
 end
 
-set(ax1, 'YScale', 'log')
-set(ax1, 'YTick', [2,5, 10,20, 50, 100], 'YLim', [0.001, 100])
+set(ax3, 'YScale', 'log')
+set(ax3, 'YTick', [2,5, 10,20, 50, 100], 'YLim', [0.001, 100])
 ylm1 = ylim();
 grid on
-ax_pos = ax1.Position;
-ax2 = axes('Position', ax_pos);
-% Fig15.CurrentAxes = ax2;
+
 
 for k=1:4
-  hands2 (k) = plot(ax2, abs(step_exps_CS.step_ref.step_diff_amps(2:end)),...
+  hands2 (k) = plot(ax3, abs(step_exps_CS.step_ref.step_diff_amps(2:end)),...
     step_exps_CS.TS_mat(:, idx(k))*1000, ms{k}, 'Color', 'r', 'MarkerSize', msize);
   hands2(k).DisplayName = step_exps_CS.step_exps{idx(k)}.name;
   hold on
 end
 ylm2 = ylim();
-set(ax2, 'YScale', 'log')
-set(ax2, 'Visible', 'off', 'YLim', [2, max([ylm1(2), ylm2(2)])]);
-set(ax1, 'Visible', 'on', 'YLim', [2, max([ylm1(2), ylm2(2)])]);
+set(ax3, 'Visible', 'on', 'YLim', [2, ylm2(2)]);
 
 grid on
-leg1 = legend(hands1);
-set(leg1, 'Box', 'off', 'Position', [0.1020 0.7220 0.3539 0.2362], 'FontSize', 8)
-pos1 = leg1.Position();
-
-leg2 = legend(hands2);
-pos2 = leg2.Position(); 
-%                                     lft              bt 
-set(leg2, 'Box', 'off', 'Position', [pos1(1)+ pos1(3), pos1(2), pos2(3:4)], 'FontSize', 8)
+leg1 = legend([hands1, hands2]);
+set(leg1, 'Units', 'inches', 'Box', 'on', 'FontSize', 8, 'NumColumns', 2)
+% pos1 = [0.1020 0.80 0.35 0.18];
+set(leg1, 'Position',  [0.45358 2.7045 2.4186 0.59062])
 
 
-Fig16.CurrentAxes = ax1
-xlabel('$|r_i - r_{i-1}|$')
-ylabel('settle time [ms]')
-%%
-if 1 %saveon
+Fig16.CurrentAxes = ax3;
+xlab = xlabel('$|r_i - r_{i-1}|$');
+ylab = ylabel('settle time [ms]');
+htit = title('(simulation)', 'FontSize', 10);
+
+tick_pad_inches = ax3.FontSize/72 + 0.0;  % 1/72 = pt
+set(xlab, 'Units', 'inches', 'HorizontalAlignment', 'center',...
+  'Position', [wd/2, -tick_pad_inches, 0])
+
+if saveon
   saveas(Fig16, fullfile(PATHS.jfig, 'ts_vs_delref_sim.svg'))
 end
 
@@ -279,11 +287,11 @@ ax1.XLabel.String = 't [ms]';
 set(ax1, 'YLim', [-7.5, 7.5]);
 
 % "best"
-annotation('ellipse', [0.18,0.78, 0.04, 0.05], 'Color', 'r');
-annotation('arrow', [0.2, 0.2], [0.785, 0.6]); %, 'Color', 'k');
+el1 = annotation('ellipse', [0.18,0.78, 0.04, 0.05], 'Color', 'r');
+a1 = annotation('arrow', [0.2, 0.2], [0.785, 0.6]); %, 'Color', 'k');
 % "worst"
-annotation('ellipse', [0.55, 0.93, 0.04, 0.05], 'Color', 'r');
-annotation('arrow', [0.57, 0.59], [0.95, 0.6]); %, 'Color', 'k');
+el2 = annotation('ellipse', [0.73 0.85 0.04, 0.05], 'Color', 'r');
+a2 = annotation('arrow', [0.74 0.7016],[0.86 0.676]); %, 'Color', 'k');
 
 % Even though we want to zoom in, its maybe easiest to stick with our framework
 % and plot the whole thing, then adjust xlim and ylim.
@@ -300,9 +308,9 @@ step_ref.plot(ax3);
 step_ref.plot_settle_boundary(ax3, TOL, tol_mode);
 hands = step_exps_CS.ploty_selected([5:8], ax3);
 
-set(ax3, 'XLim', [1.403, 1.429]);
+set(ax3, 'XLim', [1.9998, 2.0438]);
 ax3.XLabel.String = 't [ms]';
-set(ax3, 'YLim', [5, 5.6]);
+set(ax3, 'YLim', [-.05, .1]);
 
 leg = legend(hands);
 set(leg, 'FontSize', 8, 'Box', 'off',...
